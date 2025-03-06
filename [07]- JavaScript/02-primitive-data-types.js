e8425d0b9b464ccd52280936638557d16d0ae2b0
djustinandre <deleonjustinandre@gmail.com>
2025-03-06 13:00:57 +0800

    Add codes for JS.

---
+++ [07]- JavaScript/02-primitive-data-types.js
@@ -0,0 +1,27 @@
+// Strings: text data enclosed in "" or ''
+var firstName = 'John';
+console.log('First name:', firstName);
+console.log('Data Type of First name:', typeof firstName);
+
+// typeof: determine the data type of a variable
+// Number: numeric value (30, -30, 3.14)
+let age = 22;
+console.log('Age:', age);
+console.log('Data Type of Age:', typeof age);
+
+// Booleans: either true or false
+let isStudent = true;
+console.log('Is John a student?', isStudent);
+console.log('Data Type of isStudent:', typeof isStudent);
+
+// null: intentional absence of values
+let car = null;
+console.log('Car:', car);
+console.log('Data Type of car:', typeof car);
+car = 'AE86';
+console.log('Data Type of car:', typeof car);
+
+// undefined: unintentional absence of values
+let city;
+console.log('City:', city);
+console.log('Data Type of City:', typeof city);
